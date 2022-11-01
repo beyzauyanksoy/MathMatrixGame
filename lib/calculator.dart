90a106d0a01fa873c38475e36b8d4196d5212133
Beyza <beyzauyanik57@hotmail.com>
2022-11-01 14:47:45 +0300

    calculator sayfası oluşturulmaya başlandı

---
+++ lib/calculator.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import 'widgets/calculate_top_widget.dart';
+
+class Calculator extends StatefulWidget {
+  const Calculator({super.key});
+
+  @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          CalculateTopWidget()
+        ],
+      ),
+    );
+  }
+}
+
